4c6b147ac27cdf977be39d271b8a6fff1d1c4050
SattraJoe <sattra.k@kkumail.com>
2025-10-30 15:53:50 +0700

    feat: Adds route and component for Immer example
    Introduces a new route and component for demonstrating the usage of Immer for state management in React.
    
    This change includes the addition of the '/11-immer' route, along with the corresponding component that showcases various state update techniques using Immer, including `produce` and `useImmer`.
    
    Enhances the application's routing structure and provides a practical example of Immer's capabilities.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,6 +20,7 @@ function App() {
         <li><a href="/8-state" className={link}>8-state</a></li>
         <li><a href="/9-snapshot" className={link}>9-snapshot</a></li>
         <li><a href="/10-reference-types" className={link}>10-reference-types</a></li>
+        <li><a href="/11-immer" className={link}>11-immer</a></li>
       </ul>
     </>
   )
